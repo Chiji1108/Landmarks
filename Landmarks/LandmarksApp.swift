02d0da8e43cd39a9e31ec836c243663e8760843f
chiji1108 <shingo.pad@gmail.com>
2025-02-03 21:29:08 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 千々岩真吾 on 2025/02/03.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
